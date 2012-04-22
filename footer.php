376a306054ae29d3485973bdd7d20908c25c3ba6
Ross <github@hypn.za.net>
2012-04-22 12:40:26 +0300

    Previous link results in a 404 :(

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
         </div>
         
         <div class="cap"><img src="<?php echo bloginfo('template_directory');?>/images/content-bot.jpg" height="" width="" alt="decorative footer border"/></div>
-        <div class="footer">Copyright &copy; <?php echo date('Y'); ?>, <?php echo bloginfo('name');?>. Powered by Wordpress. Theme design by <a href="http://www.zacvineyard.com/d3">Zac Vineyard</a>.</div>
+        <div class="footer">Copyright &copy; <?php echo date('Y'); ?>, <?php echo bloginfo('name');?>. Powered by Wordpress. Theme design by <a href="http://www.zacvineyard.com/">Zac Vineyard</a>.</div>
     
     </div>
 	
