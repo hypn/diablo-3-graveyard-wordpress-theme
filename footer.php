0c28ff72b6f9feaebe01b92979c8f2bbb71ee6fc
Zac Vineyard <zacvineyard@gmail.com>
2012-04-23 22:04:20 -0600

    Footer link patch.

--- footer.php
+++ footer.php
@@ -3,10 +3,11 @@
         </div>
         
         <div class="cap"><img src="<?php echo bloginfo('template_directory');?>/images/content-bot.jpg" height="" width="" alt="decorative footer border"/></div>
-        <div class="footer">Copyright &copy; <?php echo date('Y'); ?>, <?php echo bloginfo('name');?>. Powered by Wordpress. Theme design by <a href="http://www.zacvineyard.com/d3">Zac Vineyard</a>.</div>
+        <div class="footer">Copyright &copy; <?php echo date('Y'); ?>, <?php echo bloginfo('name');?>. Powered by Wordpress. Theme design by <a href="http://www.zacvineyard.com">Zac Vineyard</a>.</div>
     
     </div>
 	
 </div>
+<?php wp_footer(); ?>
 </body>
 </html>
